932a235028cc286f793a588539ee172218ed5b13
trds42 <trds42@gmail.com>
2022-08-07 14:39:56 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,8 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+x = []
+for i in range(first_number, second_number):
+    if i % 3 == 0:
+        x.append(i)
+print(x)

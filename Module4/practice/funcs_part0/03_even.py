c7fac33db710db0c766985e8dd3db8009f229843
trds42 <trds42@gmail.com>
2022-08-07 16:30:04 +0300

    Update 03_even.py

--- Module4/practice/funcs_part0/03_even.py
+++ Module4/practice/funcs_part0/03_even.py
@@ -1,8 +1,11 @@
-def even(...):
-    ...
-    return ...
+def even(n):
+    if n % 2 == 0 and n != 0:
+        return True
+    else:
+        return False
 
-n = ...
+
+n = int(input('n = '))
 if even(n):
    print("Число четное")
 else:

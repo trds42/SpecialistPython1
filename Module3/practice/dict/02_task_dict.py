bcbdd8c7bdff7c7f3725f750b7965f1112272c87
trds42 <trds42@gmail.com>
2022-08-07 14:53:02 +0300

    Update 02_task_dict.py

--- Module3/practice/dict/02_task_dict.py
+++ Module3/practice/dict/02_task_dict.py
@@ -5,5 +5,11 @@ keys = ['name', 'surname', 'age', 'rate']
 values = ['Петр', 'Первый', 42, 1300]
 
 # TODO: your code here
+my_dict = {}
+i = 0
+for i in range(len(keys)):
+    my_dict[keys[i]] = values[i]
+
+print(my_dict)
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}

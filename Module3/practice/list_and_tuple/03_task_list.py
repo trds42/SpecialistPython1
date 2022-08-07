16488731b6e4fb3ca5ffad3cafebec234efbe7ab
trds42 <trds42@gmail.com>
2022-08-07 14:15:27 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,12 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+x = []
+n = int(input('n =' ))
+while n > 0:
+    x.append(int(input('Enter number: ')))
+    n -= 1
+summa = 0
+for el in x:
+    summa += el
+print(summa)

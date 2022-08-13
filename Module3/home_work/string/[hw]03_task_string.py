956ebe321560fa268b6c6ca97256ec44c0f8819b
trds42 <trds42@gmail.com>
2022-08-13 12:39:12 +0300

    Update [hw]03_task_string.py

--- Module3/home_work/string/[hw]03_task_string.py
+++ Module3/home_work/string/[hw]03_task_string.py
@@ -1,3 +1,9 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = input('text: ')
+
+count = 0
+for el in text:
+    if el not in ['а', 'у', 'о', 'и', 'э', 'ы', 'я', 'ю', 'е', 'ё']:
+        count += 1
+print(count)

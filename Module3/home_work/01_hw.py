3ae3c8b9a72018ca87191b21501b26e0220a236d
trds42 <trds42@gmail.com>
2022-08-13 13:09:58 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -4,6 +4,12 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
-
+length = len(names)
+for i in range(length):
+    if i == length - 1:
+        print(names[i])
+    else:
+        print(names[i], end=', ')
+        
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр

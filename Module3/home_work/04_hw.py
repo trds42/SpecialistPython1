efc83c97b7964fcc254bee8d02c73ec24ba28775
trds42 <trds42@gmail.com>
2022-08-13 13:29:01 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,12 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+numbers = [2, -5, 8, 9, -25, 25, 4]
+squares = []
+for number in numbers:
+    if number > 0:
+        square = number**0.5
+        if square % 1 == 0:
+            squares.append(int(square))
+print(squares)

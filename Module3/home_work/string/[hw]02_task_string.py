9079942f19fdc5f5cc0f0db226f6376b2c525bd9
trds42 <trds42@gmail.com>
2022-08-13 12:49:36 +0300

    Update [hw]02_task_string.py

--- Module3/home_work/string/[hw]02_task_string.py
+++ Module3/home_work/string/[hw]02_task_string.py
@@ -3,7 +3,6 @@
 # если слова на эту букву нет, выведите "слов на б нет"
 text = input('text: ')
 
-i = 0
 start = 0
 end = 0
 if text[0] == 'б':
@@ -11,18 +10,25 @@ if text[0] == 'б':
         if text[i] == ' ':
             end = i
             break
+    if end == 0:
+        print(text)
+    else:
+        print(text[:end])
+
 else:
     for i in range(len(text)):
         if text[i] == ' ':
             if text[i+1] == 'б':
                 start = i+1
                 break
-    if start != 0:
+    if start == 0:
+        print('No words with "б"')
+    else:
         for i in range(start, len(text)):
             if text[i] == ' ':
                 end = i
                 break
-if end == 0:
-    print('No words with "б"')
-else:
-    print(text[start:end])
+        if end == 0:
+            print(text[start:])
+        else:
+            print(text[start:end])

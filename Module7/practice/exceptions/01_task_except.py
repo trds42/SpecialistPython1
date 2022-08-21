b941edc74de25a9d80d1d5c564be7a0539e5c51f
trds42 <trds42@gmail.com>
2022-08-21 12:11:07 +0300

    Update 01_task_except.py

--- Module7/practice/exceptions/01_task_except.py
+++ Module7/practice/exceptions/01_task_except.py
@@ -4,3 +4,24 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+
+def getdata():
+    str = input('nxm: ')
+    spl = str.split("x")
+    try:
+        n = int(spl[0])
+        m = int(spl[1])
+        return n, m
+    except IndexError as ex:
+        print("Разделите цифры буквой x")
+        return getdata()
+    except ValueError as ex:
+        print("Введите цифры, разделенные буквой x")
+        return getdata()
+    except BaseException as ex:
+        print("Неверные данные, введите еще раз")
+        return getdata()
+
+
+n, m = getdata()
+print(n // m)

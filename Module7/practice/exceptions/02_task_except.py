02b07fdfa8a68d44d8952c89c95b2850c915d72b
trds42 <trds42@gmail.com>
2022-08-21 12:45:13 +0300

    Update 02_task_except.py

--- Module7/practice/exceptions/02_task_except.py
+++ Module7/practice/exceptions/02_task_except.py
@@ -35,7 +35,7 @@ if month == 2:
         print(29, "дней")
     else:
         print(28, "дней")
-elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+elif month in [1, 3, 5, 7, 8, 10, 12]:
     print(31, "день")
 else:
     print(30, "дней")

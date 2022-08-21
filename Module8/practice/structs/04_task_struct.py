b10cd214d08f1900c7e2a29732c441c90adcbc94
trds42 <trds42@gmail.com>
2022-08-21 16:37:50 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -6,3 +6,8 @@ dictionary_2 = {'c': 500, 'd': 600}
 for key, value in dictionary_2.items():
     dictionary_1[key] = value
 print(dictionary_1)
+
+dictionary_1 = {'a': 300, 'b': 400}
+dictionary_2 = {'c': 500, 'd': 600}
+dictionary_1.update(dictionary_2)
+print(dictionary_1)

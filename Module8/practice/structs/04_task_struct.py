c43a421a04951404f70cd6201311931137127d72
trds42 <trds42@gmail.com>
2022-08-21 16:36:32 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,7 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+
+for key, value in dictionary_2.items():
+    dictionary_1[key] = value
+print(dictionary_1)
